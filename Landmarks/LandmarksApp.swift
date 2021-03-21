2cc647ebbe3e7988208b63848dc2568de436695e
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-03-21 10:24:05 -0400

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Andy Do on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
