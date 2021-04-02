0e3c2157711bee890bd3fe0320f3bdcfb2f6a5b4
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-04-02 10:46:54 -0400

    add animated transitions to hike graphs data

--- Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Views/Hikes/HikeView.swift
@@ -7,9 +7,19 @@ A view displaying information about a hike, including an elevation graph.
 
 import SwiftUI
 
+extension AnyTransition {
+    static var moveAndFade: AnyTransition {
+        let insertion = AnyTransition.move(edge: .trailing)
+                    .combined(with: .opacity)
+                let removal = AnyTransition.scale
+                    .combined(with: .opacity)
+                return .asymmetric(insertion: insertion, removal: removal)
+    }
+}
+
 struct HikeView: View {
     var hike: Hike
-    @State private var showDetail = false
+    @State private var showDetail = true
 
     var body: some View {
         VStack {
@@ -27,7 +37,9 @@ struct HikeView: View {
                 Spacer()
 
                 Button(action: {
-                    self.showDetail.toggle()
+                    withAnimation() {
+                        self.showDetail.toggle()
+                    }
                 }) {
                     Image(systemName: "chevron.right.circle")
                         .imageScale(.large)
@@ -39,6 +51,7 @@ struct HikeView: View {
 
             if showDetail {
                 HikeDetail(hike: hike)
+                    .transition(.moveAndFade)
             }
         }
     }
