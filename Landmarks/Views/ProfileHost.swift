af1c0952fff291b14ffde81a90950f147c48403e
ydnaod <65431123+ydnaod@users.noreply.github.com>
2021-04-04 23:47:44 -0400

    add profile view

---
+++ Landmarks/Views/ProfileHost.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        Text("Profile for: \(draftProfile.username)")
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
